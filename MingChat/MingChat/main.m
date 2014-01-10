d749a10d000367a3846a5d582a06f856adcb32c3
mingchat <mingfeng@outlook.com>
2014-01-11 07:05:36 +0800

    This is the start point for this project.

---
+++ MingChat/MingChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MingChat
+//
+//  Created by Manfred on 14-1-11.
+//  Copyright (c) 2014年 Manfred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MCAppDelegate class]));
+    }
+}
---
+++ MingChat/MingChatTests/MingChatTests.m
@@ -0,0 +1,34 @@
+//
+//  MingChatTests.m
+//  MingChatTests
+//
+//  Created by Manfred on 14-1-11.
+//  Copyright (c) 2014年 Manfred. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MingChatTests : XCTestCase
+
+@end
+
+@implementation MingChatTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
